6c5f35cdf260d12734919d4d09cb4cbc2cd68a57
naterpotatoers <nate.garza@hotmail.com>
2022-08-31 16:06:30 -0700

    removes commented out / unused code snippets

--- src/components/Serial.tsx
+++ src/components/Serial.tsx
@@ -7,21 +7,14 @@ export default function Serial(props) {
     const [reader, setReader] = useState<ReadableStreamDefaultReader>();
     const [writer, setWriter] = useState<WritableStreamDefaultWriter>();
 
-    function timeout(delay: number) {
-        return new Promise( res => setTimeout(res, delay) );
-    }
-
     const connect = async () => {
         let newPort = await navigator.serial.requestPort();
-        // await newPort.open({ baudRate: 38400 });
         await newPort.open({ baudRate: 38400 });
         await newPort.setSignals({ dataTerminalReady: false, requestToSend: false });
         setReader(newPort.readable.getReader());
         setWriter(newPort.writable.getWriter());
         setPort(newPort);
         setIsConnected(true);
-        // await timeout(300); //for 1 sec delay
-        // await readSerial();
     }
 
     const disconnect = async () => {
@@ -50,7 +43,7 @@ export default function Serial(props) {
                 }
             })
 
-            if (serialResponse != undefined){
+            if (serialResponse != undefined) {
                 console.log("Array:", responseArray);
                 console.log("output: ", serialResponse);
                 if (serialResponse.includes("{") && serialResponse.includes("}")) {
@@ -61,7 +54,6 @@ export default function Serial(props) {
                     serialResponse = "";
                 }
             }
-
         }
     }
 
