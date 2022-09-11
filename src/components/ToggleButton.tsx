09858f87bc875ccba1318082f2aea2c60974b910
naterpotatoers <nate.garza@hotmail.com>
2022-09-11 16:24:18 -0700

    removes unused files, updates mock files with correct status examples

--- src/components/ToggleButton.tsx
+++
@@ -1,8 +0,0 @@
-import React, { useState } from 'react'
-
-export default function ToggleButton() {
-    const [toggleMode, setToggleMode] = useState(true)
-    return (
-        <button className='btn btn__primary' onClick={() => setToggleMode(!toggleMode)}>Toggle Mode</button>
-    )
-}
--- src/hooks/useToggle.tsx
+++
@@ -1,6 +0,0 @@
-import { useCallback, useState } from 'react';
-
-export const useToggle = (initialState = false) => {
-    const [state, setState] = useState(initialState);
-    return [state, setState(!state)]
-}
