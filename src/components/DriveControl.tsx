4bcfd1a45cc00672c43ea318461affaf876179fd
naterpotatoers <nate.garza@hotmail.com>
2022-09-14 20:18:13 -0700

    merge conflicts

--- src/components/DriveControl.tsx
+++ src/components/DriveControl.tsx
@@ -19,13 +19,8 @@ export default function DriveControl({ commands }) {
     useEffect(() => {
         const newWheelOrientation: number = gamepad?.buttons[6]?.value ? 0 : gamepad?.buttons[8]?.value ? 1 : gamepad?.buttons[10]?.value ? 2 : driveCommands.wheel_orientation;
         const newDriveMode: string = gamepad?.buttons[7]?.value ? "D" : gamepad?.buttons[9]?.value ? "T" : gamepad?.buttons[11]?.value ? "S" : driveCommands.drive_mode;
-<<<<<<< HEAD
         const newSpeed: number = (gamepad?.axes[1] && gamepad?.buttons[0].pressed) ? parseInt((-(gamepad?.axes[1]) * 100).toFixed(0)) : 0;
         const newAngle: number = gamepad?.axes[0] ? parseInt((gamepad?.axes[0] * 12).toFixed(0)) : driveCommands.angle;
-=======
-        const newSpeed: number = gamepad?.axes[1] && gamepad?.buttons[0].pressed ? parseInt((-(gamepad?.axes[1]) * 100).toFixed(0)) : 0;
-        const newAngle: number = gamepad?.axes[2] ? parseInt((gamepad?.axes[2] * 12).toFixed(0)) : driveCommands.angle;
->>>>>>> 03b3fab7aa4dda030a318be554cd64b05d037375
         setDriveCommands({ ...driveCommands, wheel_orientation: newWheelOrientation, drive_mode: newDriveMode, angle: newAngle, speed: newSpeed });
         handleSubmit(new Event('submit'));
     }, [gamepad]);
