5b92091535d6ebfbd57e9be2de0b347129a30ded
Jose Cortes <67614349+torta19@users.noreply.github.com>
2023-01-28 10:12:47 -0800

    updated toggle mode with MMT

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,19 @@ import Status from './components/Status';
 import MapContainer from './components/GpsMap';
 import ArmSystem from './components/ArmSystem';
 import DriveSystem from './components/DriveSystem';
-import { ArmCommandDTO, DriveCommandDTO } from './util/command-dto';
+import MMTSystem from './components/MMTSystem';
+import { ArmCommandDTO, DriveCommandDTO, MMTCommandDTO } from './util/command-dto';
 import { armStringFormat, driveStringFormat } from './util/command-formats';
 import { DEFAULT_ARM_COMMANDS, DEFAULT_DRIVE_COMMANDS } from './util/constants';
+
 
 function App() {
   const commands = useRef<string>(driveStringFormat(DEFAULT_DRIVE_COMMANDS));
   const [isDriveControl, setIsDriveControl] = useState(true)
+  const [isArmControl, setIsArmControl] = useState(false)
   const [isSerial, setIsSerial] = useState(true);
   const [status, setStatus] = useState<ArmCommandDTO | DriveCommandDTO>();
+
 
   useEffect(() => {
     commands.current = isDriveControl ? driveStringFormat(DEFAULT_DRIVE_COMMANDS) : armStringFormat(DEFAULT_ARM_COMMANDS);
@@ -24,13 +28,27 @@ function App() {
   return (
     <div id="app">
       <header className='btn-group'>
-        <button className='btn btn__primary' onClick={() => setIsDriveControl(!isDriveControl)}>Toggle Mode</button>
+        
+          <select className='btn btn__primary' >  
+
+              <option className='btn btn__primary' onClick={() => {setIsDriveControl(true); setIsArmControl(false)}}>Drive System</option>
+              <option className='btn btn__primary' onClick={() => {setIsDriveControl(false); setIsArmControl(false)}}>MMT System</option> 
+              
+              <option className='btn btn__primary' onClick={() => {setIsDriveControl(false); setIsArmControl(true)}}>Arm System</option>
+                        
+          </select>
+        
+        
+        
+        
         <button className='btn btn__primary' onClick={() => setIsSerial(!isSerial)}>Toggle Connection Type</button>
         {isSerial ? <Serial commands={commands} setStatus={setStatus} isDriveControl={isDriveControl} /> : <Wifi commands={commands} setStatus={setStatus} />}
       </header>
+      
 
       <div className="grid-container">
-        {isDriveControl ? <DriveSystem commands={commands} /> : <ArmSystem commands={commands} />}
+        {isDriveControl ? <DriveSystem commands={commands} /> : 
+        isArmControl ? <ArmSystem commands={commands} /> : <MMTSystem /> }
         <Status status={status} />
         <Camera name="0" src="http://raspberrypi:8000/stream.mjpg" />
         <Camera name="1" src="http://raspberrypi:8001/stream.mjpg" />
