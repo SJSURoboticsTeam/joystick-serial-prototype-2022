eb93a279ed86989b07c0b56f0d83dfdfb8c00f18
Jose Cortes <67614349+torta19@users.noreply.github.com>
2023-02-15 22:19:05 -0800

    initial ui science work

--- src/components/Science.tsx
+++ src/components/Science.tsx
@@ -1,7 +1,28 @@
 
+import { FooterButtons } from './Forms/ControlForm';
+import { useState } from 'react'
+import { userInfo } from 'os';
 
 export default function Science () {
+    const [mode, setMode] = useState(false);
+    const [emergency, setEmergency] = useState(false);
+    const [start, setStart] = useState(false);
+    const [step, setStep] = useState(0);
+
     return (
-        <h2>Science</h2>
+        <div>
+            <h2>Science</h2>
+            <form onSubmit={(e) => { e.preventDefault() }}>
+            <div className='btn-group footer'>
+                <button className='btn btn__primary' onClick={() => setStart(!start)}>Start</button>
+                <button className='btn btn__primary' onClick={() => setEmergency(!emergency)}>emergency</button>
+            </div>
+            <div className='btn-group footer'>
+                <button className='btn btn__primary' onClick={() => setMode(!mode)}>
+                    {mode? "auto" : 'manual'}
+                </button>
+            </div>
+            </form>
+        </div>
     )
 }
