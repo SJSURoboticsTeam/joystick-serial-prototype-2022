33ae65c85d065657dfd31d28fb4241dc0b1078f9
ahmed <ahmed@MacBook-Pro.local>
2023-04-17 18:20:29 -0700

    added functionality for the overlay button which hides and reveals buttons

--- src/components/OperationalView1.tsx
+++ src/components/OperationalView1.tsx
@@ -2,56 +2,55 @@
 //side camera panels
 //return
 //mode, speed, angle
-import React from "react";
+import React, {useState} from "react";
 import Camera from '../components/Camera';
 import Status from '../components/Status';
-import "./OperationalView1.css";
-
+//import "./OperationalView1.css";
 function App(props) {
+  const [isOpened, setIsOpened] = useState(false);
+  const[show,toggleshow]=useState(true); 
+ 
+  function toggle() {
+    setIsOpened(wasOpened => !wasOpened);
+  }
     return (
       <div className="OperationalView">
-        <header className="Main-View-Header">
-          <Camera name="0" src="http://raspberrypi:8000/stream.mjpg" />
+        {/* <header className="Main-View-Header">
+          <Camera name="0" src="http://raspberrypi:8000/stream.mjpg" /> */}
+          <h2>
+            Operational View 1
+          </h2>
           <h5>
-            Main View
-          </h5>
-        </header>
-        <div className="status">
-          <h3>
             Speed:
             ,Angle:
             ,mode:
+          </h5>
+          <h3>
+            Main View
           </h3>
-        </div>
+        {/* </header> */}
         <div className="Overlay-btn">
-          <button type="button" className="btn-toggle" aria-pressed="true">
+          <button type="button" className="btn-toggle" aria-pressed="true" onClick={()=>toggleshow(!show)}>
+            {/* {show ? "hide" : "show"} */}
             Overlay
           </button>
-        </div>
-  {/* Overlay components */}
-        <div className="Camera-grid">
-          <button type="button" className="cam1-toggle" aria-pressed="true">
+          
+          {show && <button type="button" className="cam1-toggle" aria-pressed="true">
             <Camera name="1" src="http://raspberrypi:8001/stream.mjpg" />
             <h5>Camera 1</h5>
-          </button>
-          <button type="button" className="cam2-toggle" aria-pressed="true">
+          </button>}
+          {show && <button type="button" className="cam2-toggle" aria-pressed="true">
             <Camera name="2" src="http://raspberrypi:8002/stream.mjpg" />
             <h5>Camera 2</h5>
-          </button>
-          <button type="button" className="cam3-toggle" aria-pressed="true">
+          </button> }
+          {show &&  <button type="button" className="cam3-toggle" aria-pressed="true">
              <Camera name="3" src="http://raspberrypi:8003/stream.mjpg" />
             <h5>Camera 3</h5>
-          </button>
-        </div>
-        <div className="Return-btn">
-          <button type="button" className="btn-toggle" aria-pressed="true">
-            Back to main view
-          </button>
-        </div>
-        <div className="Return-pty-btn">
-          <button type="button" className="returnPty-toggle" aria-pressed="true">
+          </button>}
+          {show && <button type="button" className="returnPty-toggle" aria-pressed="true">
             Return to Prototype
-          </button>
+          </button>}
+          
         </div>
       </div>
     );
