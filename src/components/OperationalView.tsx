635fc12d589b7292ee0815b778a297ad51ccc1f6
Michael Chan <mchan100work@gmail.com>
2023-07-25 20:21:22 -0700

    Fixed aspect ratio on Main Camera

--- src/components/OperationalView.tsx
+++ src/components/OperationalView.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Menu from "./OperationalView/Menu";
 import Camera from "./Camera";
 import OVStatus from "./OperationalView/OVStatus";
+import MainCameraTest from "../Main Camera Test.png"
 
 export default function OperationalView({ commands, status }) {
   const [showOverlay, setShowOverlay] = useState(false)
@@ -11,16 +12,9 @@ export default function OperationalView({ commands, status }) {
         <OVStatus status = {status}/> 
       </div>
       <div id="ov-main-camera"> 
-        Camera
       </div>
       <Menu />
       {/* <Camera name="primary" src="http://192.168.1.153:8081/" /> */}
     </div>
   );
 }
-
-// // {showOverlay && <Menu />}
-//       {/* <button id="ov-hide-overlay" type="button" className="btn-toggle" onClick={() => setShowOverlay(!showOverlay)}>
-//         {showOverlay ? "Hide " : "Show "}
-//         Overlay
-//       </button> */}
