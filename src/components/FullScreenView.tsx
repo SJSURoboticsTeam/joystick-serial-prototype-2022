a60ef196606ec23c50e552a8d54311713f6b3baf
Michael Chan <mchan100work@gmail.com>
2023-10-05 01:46:13 -0700

    fixed bug where cameras do not show up when the webserver is on for the first time

--- src/components/FullScreenView.tsx
+++ src/components/FullScreenView.tsx
@@ -2,6 +2,7 @@
 import MainCamera from "./OperationalView/MainCamera";
 
 export default function FullScreen({camNum}) {
+  console.log(camNum)
   const displayCam = Array.from({length: camNum}, (_, index) => {
     return <MainCamera key={index}/>
   }); 
--- src/components/OperationalView.tsx
+++ src/components/OperationalView.tsx
@@ -3,15 +3,14 @@ import FullScreen from "./FullScreenView";
 import { useState } from "react";
 
 export default function OperationalView({ commands, status }) {
-  const [fullScreen, setFullScreen] = useState(false)
-  const [camNum, setCamNum] = useState(null)
+  const [camNum, setCamNum] = useState(1)
 
   return(
     <div className="operational-view">
       {/* left */}
       <div id = "ov-tools">
         <OVStatus status = {status}/>
-        <select className="cam-num"  onChange={(e) => {setCamNum(e.target.value)}}>
+        <select className="cam-num"  onChange={(e) => {setCamNum(parseInt(e.target.value))}}>
           <option className='btn btn__primary' value={1}>1 cam</option>
           <option className='btn btn__primary' value={2}>2 cam</option>
           <option className='btn btn__primary' value={3}>3 cam</option>
